7c0baa8c585a0995afb49294b74faf6a2e5b0bfa
Josdvin Macario <jmacario-2017497@kinal.edu.gt>
2022-07-13 02:16:49 -0600

    Heroku

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 'use strict'
 
+require('dotenv').config()
+//console.log(process.env)
+
 var mongoose = require('mongoose');
 var app = require('./app');
 var user = require('./controllers/user.controller')
@@ -7,11 +10,13 @@ var port = 3800;
 
 mongoose.Promise = global.Promise;
 mongoose.set('useFindAndModify', false);
-mongoose.connect('mongodb://localhost:27017/GestiondeHotelesG4', {useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(process.env.DATABASE_CONNECT , {useNewUrlParser: true, useUnifiedTopology: true})
     .then(()=>{
         user.initAdmin();
         console.log('connect to DB');
-        app.listen(port, ()=>{
+        app.listen(process.env.PORT || port, ()=>{
             console.log('server express running')
         })
     }).catch((err)=>console.log('connection error to DB', err))
+
+    
